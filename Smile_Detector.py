019a74cd2c553bf58520890bfd3b0066423f1754
Luis Fernando <nando.lfp@hotmail.com>
2022-02-20 13:08:50 -0300

    finished

--- Smile_Detector.py
+++ Smile_Detector.py
@@ -29,17 +29,24 @@ while True:
         # draw a rectangle around the face
         cv2.rectangle(frame, (x, y), (x+w, y+h), (100, 200, 50), 2)
 
-        the_face = (x, y, w, h)
+        # get the sub frame
+        the_face = frame[y:y+h, x:x+w]
+        
         # Change to grayscale
         face_grayscale = cv2.cvtColor(the_face, cv2.COLOR_BGR2GRAY)
 
         smiles = smile_detector.detectMultiScale(face_grayscale, scaleFactor=1.7, minNeighbors=20)
 
         # find all smiles in the face
-        for (x_, y_, w_, h_) in smiles:
+        #for (x_, y_, w_, h_) in smiles:
 
             # draw a rectangles around the smile
-            cv2.rectangle(frame, (x_, y_), (x_ + w_, y_ + h_), (50, 50, 200), 2)
+            #cv2.rectangle(the_face, (x_, y_), (x_ + w_, y_ + h_), (50, 50, 200), 2)
+
+        #Label this face as smaling
+        if len(smiles) > 0:
+            cv2.putText(frame, 'smiling', (x, y+h+40), fontScale=3,
+            fontFace=cv2.FONT_HERSHEY_PLAIN, color=(255, 255, 255))
 
     # show the current frame
     cv2.imshow('Smile Detector', frame)

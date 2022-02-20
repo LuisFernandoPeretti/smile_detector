87474f30ad0e8a3ba89d692c0844dc47a8078bd0
Luis Fernando <nando.lfp@hotmail.com>
2022-02-20 12:19:08 -0300

    save

--- Smile_Detector.py
+++ Smile_Detector.py
@@ -2,8 +2,57 @@ import cv2
 
 #Face classifier
 face_detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+smile_detector = cv2.CascadeClassifier('haarcascade_smile.xml')
 
-print(face_detector)
+# grab webcam feed
+webcam = cv2.VideoCapture(0)
+
+# show the current frame
+while True:
+
+    #read the current frame from webcam video stream
+    successful_frame_read, frame = webcam.read()
+
+    # if tere's an error, abort
+    if not successful_frame_read:
+        break
+
+    # Change to grayscale
+    frame_grayscale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # detect faces first
+    faces = face_detector.detectMultiScale(frame_grayscale)
+
+    # run face detection within each of those face
+    for (x, y, w, h) in faces:
+        
+        # draw a rectangle around the face
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (100, 200, 50), 2)
+
+        the_face = (x, y, w, h)
+        # Change to grayscale
+        face_grayscale = cv2.cvtColor(the_face, cv2.COLOR_BGR2GRAY)
+
+        smiles = smile_detector.detectMultiScale(face_grayscale, scaleFactor=1.7, minNeighbors=20)
+
+        # find all smiles in the face
+        for (x_, y_, w_, h_) in smiles:
+
+            # draw a rectangles around the smile
+            cv2.rectangle(frame, (x_, y_), (x_ + w_, y_ + h_), (50, 50, 200), 2)
+
+    # show the current frame
+    cv2.imshow('Smile Detector', frame)
+
+    # display
+    cv2.waitKey(1)
+
+# cleanup
+webcam.release()
+cv2.destroyAllWindows()
+
+
+print('ok')
 
 
 
